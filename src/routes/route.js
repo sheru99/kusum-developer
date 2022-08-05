380c6fbcc537cb8695cc85ec0cea5e16f9762255
Shayan Biswas <info.sbriju@gmail..com>
2022-08-05 16:20:29 +0530

    second

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,6 +8,7 @@ router.get('/test-me', function (req, res) {
     res.send('My second ever api!')
 });
 
+<<<<<<< HEAD
 
 router.get('/test-you', function(req, res){
     res.send('This is the second routes implementation')
@@ -18,3 +19,11 @@ router.get('/give-me-students-data',function(req, res){
 })
 module.exports = router;
 // adding this comment for no reason
+=======
+router.get('/hello',function(rec,res){
+    res.send('Hello Welcome')
+    console.log('hi')
+});
+
+module.exports = router;
+>>>>>>> 9057bcb (first)
