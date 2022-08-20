f252ebdc0c9597375774bdd9d6dbb6a3c6abf005
kusum <kushay317@gmail.com>
2022-08-20 23:53:35 +0530

    progress

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://kusum_99:9vJ9mxlJH1cYZ1oO@cluster0.jelghm1.mongodb.net/refPopulate-db", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const authorController= require("../controllers/authorController")
-const bookController= require("../controllers/bookController")
+const authorController = require("../controllers/authorController")
+const bookController = require("../controllers/bookController")
+const publisherController = require("../controllers/publisherController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+/////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
-router.post("/createAuthor", authorController.createAuthor  )
+router.post("/registerAuthor", authorController.registerAuthor)
 
-router.get("/getAuthorsData", authorController.getAuthorsData)
+///////////////////////////////////////////////////////////////////////////////////////
+router.post("/registerPublisher", publisherController.registerPublisher)
+/********************************register****************************** */ 
+router.post("/registerBook", bookController.registerBook)
 
-router.post("/createBook", bookController.createBook  )
+router.get("/getBooks", bookController.getBooks)
 
-router.get("/getBooksData", bookController.getBooksData)
-
-router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
 
 module.exports = router;
